2eece87700fa88d3038392b633f1a047a322df92
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-05-18 19:03:57 +0100

    Added setup.py

---
+++ controldiffeq/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+import setuptools
+
+
+here = pathlib.Path(__file__).resolve().parent
+
+
+with open(here / 'README.md', 'rb') as f:
+    readme = f.read()
+
+
+setuptools.setup(name='controldiffeq',
+                 version='0.0.1',
+                 author='Patrick Kidger',
+                 author_email='contact@kidger.site',
+                 maintainer='Patrick Kidger',
+                 maintainer_email='contact@kidger.site',
+                 description='PyTorch functions for solving CDEs.'
+                 long_description=readme,
+                 url='https://github.com/patrick-kidger/NeuralCDE/tree/master/controldiffeq',
+                 license='Apache-2.0',
+                 zip_safe=False,
+                 python_requires='>=3.5, <4',
+                 install_requires=['torch>=1.0.0'],
+                 classifiers=["Programming Language :: Python :: 3",
+                              "License :: OSI Approved :: Apache Software License"])
